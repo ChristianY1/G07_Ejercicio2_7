75e6a9b88b1a10e4bcfe83183bb1803909402a3b
ChristianY1 <christianyunga@gmail.com>
2020-07-02 22:36:03 -0500

    caja completa

--- Resolucion/src/ec/edu/ups/Entidades/CuentaContable.java
+++ Resolucion/src/ec/edu/ups/Entidades/CuentaContable.java
@@ -13,8 +13,16 @@ public class CuentaContable implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigo;
+	@ManyToOne
+	@JoinColumn
+	private Caja caja;
 
 	public CuentaContable() {
+		
 		super();
 	}
    
