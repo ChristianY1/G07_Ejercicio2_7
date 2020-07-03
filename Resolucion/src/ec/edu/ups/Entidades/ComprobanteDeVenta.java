c5ef8752a02efa4b65ae69be1de8ceec47b62b14
ChristianY1 <christianyunga@gmail.com>
2020-07-02 22:18:33 -0500

    Ajuste Comprobante de venta

---
+++ Resolucion/src/ec/edu/ups/Entidades/ComprobanteDeVenta.java
@@ -0,0 +1,21 @@
+package ec.edu.ups.Entidades;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: ComprobanteDeVenta
+ *
+ */
+@Entity
+
+public class ComprobanteDeVenta implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public ComprobanteDeVenta() {
+		super();
+	}
+   
+}
