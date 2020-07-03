1390c3a5887b327c0a7a930caee62f7d9232cc58
ChristianY1 <christianyunga@gmail.com>
2020-07-02 22:17:10 -0500

    clases

---
+++ Resolucion/src/ec/edu/ups/Entidades/Caja.java
@@ -0,0 +1,21 @@
+package ec.edu.ups.Entidades;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Caja
+ *
+ */
+@Entity
+
+public class Caja implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Caja() {
+		super();
+	}
+   
+}
